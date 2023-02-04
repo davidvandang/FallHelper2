8ebf20b0b6f46df0e74d315410acbddfa8927df4
davidvandang <98755624+davidvandang@users.noreply.github.com>
2023-02-04 15:44:15 +0000

    UI design updated

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -1,30 +1,51 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import javax.annotation.Nullable;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "CallDatabase.db";
-    public static final String TABLE_NAME = "calls_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "Date";
-    public static final String COL_3 = "Time";
-    public DatabaseHelper (Context context) {
-        super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+    private Context context;
+    private static final String DATABASE_NAME = "CallDatabase.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "calls_table";
+    private static final String COL_ID = "ID";
+    private static final String COL_DATE = "Date";
+    private static final String COL_TIME = "Time";
+    public DatabaseHelper (@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME +" (ID INTERGER PRIMARY KEY AUTOINCREMENT, COL_1 DATE, COL_3 TIME)");
-
+        String query =
+                "CREATE TABLE " + TABLE_NAME + " (" + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                       COL_ID + " TEXT, " +
+                        COL_DATE + " TEXT, " +
+                        COL_TIME + " TEXT);";
+        db.execSQL(query);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int i, int ii) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    Cursor readAllData() {
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db =  this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query,null);
+        }
+        return cursor;
     }
 }
